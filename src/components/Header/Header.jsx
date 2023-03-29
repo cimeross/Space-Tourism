e1436c4e04acf02e770dda618a82220c901b56e4
Milan Suvajac <milansuvajac021@gmail.com>
2023-03-29 22:10:43 +0200

    feat: Added responsive menu navigation(dropdown)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,26 @@
-import React from "react";
 import logo from "../../assets/shared/logo.svg";
 import "./header.scss";
 import { Link } from "react-router-dom";
+import menuOpen from "../../assets/shared/icon-hamburger.svg";
+import { useState } from "react";
 
 function Header() {
+	const [isNavOpen, setIsNavOpen] = useState(false);
+
+	const toggleNav = () => {
+		setIsNavOpen(!isNavOpen);
+	};
+
 	return (
-		<>
+		<div>
 			<header className="header">
 				<div className="logo">
 					<Link to="/">
 						<img src={logo} alt="logo" />
 					</Link>
 				</div>
-				<nav className="navbar">
-					<ul>
+				<nav className={isNavOpen ? "navbar active" : "navbar"}>
+					<ul className="nav-links">
 						<li>
 							<Link to="/">
 								<span>00</span> Home
@@ -36,8 +43,18 @@ function Header() {
 						</li>
 					</ul>
 				</nav>
+
+				<div className="hamburger" onClick={toggleNav}>
+					<button className="menu-btn">
+						<img
+							src={menuOpen}
+							alt="menu icon"
+							className={isNavOpen ? "active" : ""}
+						/>
+					</button>
+				</div>
 			</header>
-		</>
+		</div>
 	);
 }
 
